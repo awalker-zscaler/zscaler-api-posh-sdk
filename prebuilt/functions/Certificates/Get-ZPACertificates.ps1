d5bd413dc4d464409b2d4fb6b1fa530ee2458640
awalker-zscaler <126690666+awalker-zscaler@users.noreply.github.com>
2023-04-25 10:25:40 -0400

    Create Get-ZPACertificates.ps1

---
+++ prebuilt/functions/Certificates/Get-ZPACertificates.ps1
@@ -0,0 +1,44 @@
+Function Get-ZPACertificates{
+    <#
+    .SYNOPSIS
+        Gets all configured Certificates for your Tenant.
+
+    .DESCRIPTION
+        A function that returns a list of all configured certificates for your ZScaler ZPA Tenant
+
+    .PARAMETER token
+        The token that was returned on your authenticated session.
+
+    .PARAMETER customerid
+        The unique identifier of your ZPA Tenant.
+
+    .PARAMETER zscalercloud
+        The cloud that your tenant resides in.
+
+    .OUTPUTS
+        [System.Management.Automation.PSCustomObject] List of Certificates
+
+    .EXAMPLE
+        $certificates = Get-ZPACertificates -token $token -customerid $customerid -zscalercloud config.zpagov.net
+
+    #>
+    [CmdletBinding()]
+    param(
+        [Parameter(Mandatory, Position=0)]$token, # Your Authenticated ZPA Token
+        [Parameter(Mandatory, Position=1)]$customerid, # The unique identifier of your ZPA Tenant
+        [Parameter(Mandatory, Position=2)]$zscalercloud # The ZScaler Cloud of your tenant
+    )
+    $apiurl = "https://$($zscalercloud)/mgmtconfig/v1/admin/customers/$($customerid)/certificate"
+    $response = Invoke-RestMethod -URI "$($apiurl)?page=1&pagesize=50" -Method Get -ContentType "*/*" -Headers @{ Authorization = "Bearer $token"}
+    IF($response.totalPages -eq "1"){
+        return $response.list
+    }ELSEIF($response.totalPages -gt "1"){
+        $list = $response.list
+        2..$($response.totalPages) | ForEach-Object{
+            $list += (Invoke-RestMethod -URI "$($apiurl)?page=$($_)&pagesize=50" -Method Get -ContentType "*/*" -Headers @{ Authorization = "Bearer $token"}).list
+        }
+        return $list
+    }ELSE{
+        return $null
+    }
+}
